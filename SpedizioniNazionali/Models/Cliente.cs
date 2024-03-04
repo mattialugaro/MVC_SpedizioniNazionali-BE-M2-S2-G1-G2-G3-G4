de00aba9dd1ef9d4a1b7bf38befe9cc4637e3c90
mattia lugaro <mattialugaro@gmail.com>
2024-03-04 19:22:37 +0100

    add check tipoCliente

--- SpedizioniNazionali/Models/Cliente.cs
+++ SpedizioniNazionali/Models/Cliente.cs
@@ -23,11 +23,17 @@ namespace SpedizioniNazionali.Models
         [StringLength(50, ErrorMessage = "L' Email non può avere più di 50 caratteri")]
         public string Email { get; set; }
 
-        [StringLength(50, ErrorMessage = "Il Codice Fiscale non può avere più di 16 caratteri")]
+        [Required(ErrorMessage = "Il Tipo di Cliente è un campo obbligatorio")]
+        [StringLength(7, ErrorMessage = "Il Tipo di Cliente non può avere più di 7 caratteri")]
+        [checkTipoCliente(Type = "Privato,Azienda", ErrorMessage = "Il Tipo di Cliente può essere solo Privato o Azienda.")]
+        [DisplayName("Tipo di Cliente (Privato o Azienda)")]
+        public string TipoCliente { get; set; }
+
+        [StringLength(16, ErrorMessage = "Il Codice Fiscale non può avere più di 16 caratteri")]
         [DisplayName("Codice Fiscale (Solo per i Privati)")]
         public string CodiceFiscale {  get; set; }
 
-        [StringLength(50, ErrorMessage = "La Partita Iva non può avere più di 11 caratteri")]
+        [StringLength(11, ErrorMessage = "La Partita Iva non può avere più di 11 caratteri")]
         [DisplayName("Partita Iva (Solo per le Aziende)")]
         public string PIva {  get; set; }
 
---
+++ SpedizioniNazionali/checkTipoCliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SpedizioniNazionali
+{
+    public class checkTipoCliente : ValidationAttribute
+    {
+        public string Type { get; set; }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string[] Types = Type.ToString().Split(',');
+            if(Types.Contains(value.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("Il Tipo di Cliente può essere solo Privato o Azienda.");
+            }
+        }
+    }
+}
